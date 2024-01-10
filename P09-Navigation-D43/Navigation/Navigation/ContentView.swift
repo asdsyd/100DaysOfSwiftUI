ceb6823b4516c0bc95c5ea4032b9a06209bf9454
asdsyd <ashasd97@gmail.com>
2024-01-10 13:20:09 +0530

    P09-MoonShot

---
+++ P09-Navigation-D43/Navigation/Navigation/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Asad Sayeed on 09/01/24.
+//
+
+import SwiftUI
+
+struct Student: Hashable
+{
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
+struct DetailView: View
+{
+    var number: Int
+    
+    var body: some View 
+    {
+        Text("Detail View \(number)")
+    }
+    
+    init(number: Int) 
+    {
+        self.number = number
+        print("Creating detail view \(number)")
+    }
+}
+
+struct ContentView: View 
+{
+    @State private var path = NavigationPath()
+    
+    var body: some View
+    {
+        NavigationStack(path: $path)
+        {
+            List
+            {
+                ForEach(0..<5)
+                { i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+                
+                ForEach(0..<5)
+                { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            .toolbar
+            {
+                Button("Push 556")
+                {
+                    path.append(556)
+                }
+                
+                Button("Push hello")
+                {
+                    path.append("hello")
+                }
+            }
+            .navigationDestination(for: Int.self)
+            { selection in
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self)
+            { selection in
+                Text("You selected the string \(selection)")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P09-Navigation-D43/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Asad Sayeed on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
