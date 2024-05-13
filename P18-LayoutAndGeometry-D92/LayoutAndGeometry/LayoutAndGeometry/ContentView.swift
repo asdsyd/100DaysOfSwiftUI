ebe069b3ff677b2d7d11e1543e78f751d6fb7b5c
asdsyd <ashasd97@gmail.com>
2024-05-13 16:17:16 +0530

    P18-LayoutAndGeometry

--- P18-LayoutAndGeometry-D92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ P18-LayoutAndGeometry-D92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { number in
+                        Text("Number \(number)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .frame(width: 200, height: 200)
+                            .visualEffect { content, geometryProxy in
+                                content
+                                    .rotation3DEffect(
+                                        .degrees(-geometryProxy.frame(in: .global).minX / 8),
+                                        axis: (x: 0, y: 1, z: 0)
+                                    )
+                            }
+                }
+            }
+            .scrollTargetLayout()
         }
-        .padding()
+        .scrollTargetBehavior(.viewAligned)
     }
 }
 
